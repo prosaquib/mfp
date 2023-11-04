dc601b0e556b9cec6d8ceca0e74376b6253a1602
prosaquib <saquib.geek@gmail.com>
2023-11-04 12:27:25 +0530

    App js

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 export default () => {
     return (
         <div>
-            <h1>Welcome to container App</h1>
+            <h1>Welcome to the container App</h1>
         </div>
     )
 }
