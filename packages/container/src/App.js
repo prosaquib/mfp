8155bab9d2cfb5765a4ad5d7cdb9b31bb20522a3
prosaquib <saquib.geek@gmail.com>
2023-11-06 21:29:24 +0530

    To run workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 export default () => {
     return (
         <div>
-            <h1>Welcome to the container App</h1>
+            <h1>Welcome to the container Application</h1>
         </div>
     )
 }
